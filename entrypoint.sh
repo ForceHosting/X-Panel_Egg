d8400cc01a1c210bdf367c917d5b94e212829e5c
The MG | GallearTech <63264652+The-MG@users.noreply.github.com>
2023-08-19 19:31:11 -0700

    Prepare for SaaS

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,30 +5,23 @@ cd /home/container || exit 1;
 if [[ -f "/home/container/index.js" ]]; then
   npm run start:prod
 else
-  echo "X-Panel was not found to be installed. Would you like to install it? (Y/n)";
-  read proceed;
+  echo "X-Panel was not found to be installed. Installing now....";
 
-  case "$proceed" in
-    "Y"|"y")
         echo "Building and deploying your X-Panel instance...";
 
-        git clone --branch main https://github.com/ForceHosting/X-Panel.git /home/container/xpanel
-        rm -rf /home/container/xpanel/frontend
+        git clone --branch saas https://github.com/ForceHosting/X-Panel.git /home/container/xpanel
+        mv /home/container/saas.json /home/container/xpanel/frontend/src/saas.json
+        cd /home/container/xpanel/frontend && npm install --legacy-peer-deps && npm run build
+        
         mv /home/container/xpanel/server/** /home/container
+        mkdir /home/container/site
+        mv /home/container/xpanel/frontend/build/** /home/container/site
         mv /home/container/xpanel/server/.env /home/container
         rm -rf /home/container/xpanel
-        mv /home/container/config.json.example /home/container/config.json
+        mv /home/container/pterodactyl.json /home/container/config.json
 
         npm install
 
         echo "X-Panel was successfully deployed! Please open your config.json and start configuring the deployment. To build the frontend, please download the frontend files, modify them as you wish, then run npm run build. Once built, drag the contents of the build folder into a folder called 'site' within your deployment.";
         exit 0;
         ;;
-    "N"|"n")
-        exit 0;
-        ;;
-    *)
-        exit 1;
-        ;;
-    esac
-fi
